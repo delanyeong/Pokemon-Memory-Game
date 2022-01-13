c8f41f30fcd8e7805e782511b18b208489f18b14
delan <delan.april@gmail.com>
2022-01-14 03:20:30 +0800

    timer still counting after winning pt1

--- app.js
+++ app.js
@@ -102,6 +102,7 @@ function checkForMatch() {
     resultDisplay.textContent = cardsWon.length
     if  (cardsWon.length === cardArray.length/2) {
       resultDisplay.textContent = 'Congratulations! You found them all!'
+      reset();
     }
   }
 
@@ -150,38 +151,43 @@ function flipCard() {
 
   //timer
   function startTimer(duration, display, callback) {
-  var timer = duration,
-    minutes, seconds;
-
-  var myInterval = setInterval(function() {
-    minutes = parseInt(timer / 60, 10)
-    seconds = parseInt(timer % 60, 10);
-
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    display.textContent = minutes + ":" + seconds;
-
-    if (--timer < 0) {
-      timer = duration;
-      
-      // clear the interal
-      clearInterval(myInterval);
-
-      // use the callback
-      if(callback) {
-          callback();
+    var timer = duration,
+      minutes, seconds;
+  
+    var myInterval = setInterval(function() {
+      minutes = parseInt(timer / 60, 10)
+      seconds = parseInt(timer % 60, 10);
+  
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+  
+      display.textContent = minutes + ":" + seconds;
+  
+      if (--timer < 0) {
+        timer = duration;
+        
+        // clear the interal
+        clearInterval(myInterval);
+  
+        // use the callback
+        if(callback) {
+            callback();
+        }
       }
-    }
-  }, 1000);
-}
-
-window.onload = function() {
-  var time = 5,
-    display = document.querySelector('#time');
-  startTimer(time, display, function() { alert('done'); });
-};
-
+    }, 1000);
+  }
+  
+  window.onload = function() {
+    var time = 300,
+      display = document.querySelector('#time');
+    startTimer(time, display, function() { alert("Time's Up!"); });
+  };
+  
+  function reset() {
+    var time = 0,
+      display = document.querySelector('#time');
+    startTimer(time, display, function() { alert("Congrats!"); });
+  };
 
 
 
