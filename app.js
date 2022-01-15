de4bf73cf9b53c17d7163286d7c3c5d3f71e26b9
delan <delan.april@gmail.com>
2022-01-15 23:02:47 +0800

    moved progress bar constr above bc it was unable to be read

--- app.js
+++ app.js
@@ -52,6 +52,36 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     ]
 
+    //Constructor for Healthbar
+  class ProgressBar {
+      constructor (element, initialValue = 0) {
+          this.valueElem = element.querySelector('.progress-bar-value');
+          this.fillElem = element.querySelector('.progress-bar-fill');
+
+          this.setValue(initialValue);
+      }
+
+      setValue (newValue) {
+          if (newValue < 0) {
+              newValue = 0
+          }
+
+          if (newValue > 100) {
+              newValue = 100;
+          }
+          this.value = newValue
+          this.update();
+        }
+
+        update() {
+            const percentage = this.value + '%';
+
+            this.fillElem.style.width = percentage;
+            this.valueElem.textContent = percentage;
+        }
+
+  }
+
 const grid = document.querySelector('.grid')
 const resultDisplay = document.querySelector('#result')
 let cardsChosen = []
@@ -117,36 +147,6 @@ function flipCard() {
         setTimeout(checkForMatch, 500)
     }
 }
-
-//Constructor for Healthbar
-  class ProgressBar {
-      constructor (element, initialValue = 0) {
-          this.valueElem = element.querySelector('.progress-bar-value');
-          this.fillElem = element.querySelector('.progress-bar-fill');
-
-          this.setValue(initialValue);
-      }
-
-      setValue (newValue) {
-          if (newValue < 0) {
-              newValue = 0
-          }
-
-          if (newValue > 100) {
-              newValue = 100;
-          }
-          this.value = newValue
-          this.update();
-        }
-
-        update() {
-            const percentage = this.value + '%';
-
-            this.fillElem.style.width = percentage;
-            this.valueElem.textContent = percentage;
-        }
-
-  }
 
   var myInterval = null;
   //timer
